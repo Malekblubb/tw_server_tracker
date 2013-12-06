79b57bd8f0c0956a20a120d51484a8f1c34d4aeb
Christoph Malek <malekchristoph@gmail.com>
2013-12-06 17:29:06 +0100

    main.cpp

---
+++ server/src/twst/main.cpp
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#include <twst/tracker.hpp>
+
+
+
+int main()
+{
+	twst::tracker tracker;
+	return tracker.run();
+}
+
